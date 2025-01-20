e064911e66c0cb334d50e488a54e0434d2879f3d
IbnuTopanAdib <resyaderesya@gmail.com>
2025-01-20 14:01:53 +0700

    first commit, add colors and theme data

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xffD72638); // Merah tegas untuk identitas utama
+  static const lightBackground = Color(0xffFCEFF1); // Merah muda lembut untuk latar terang
+  static const darkBackground = Color(0xff2C0E14); // Merah gelap untuk mode gelap
+  static const grey = Color(0xffB2A5A5); // Abu-abu lembut untuk teks sekunder
+  static const darkGrey = Color(0xff4F3434); // Abu-abu kehitaman dengan sentuhan merah
+}
+
