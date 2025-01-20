8ac406d34b238bf2627a30d30a5bce98183b7ad2
IbnuTopanAdib <resyaderesya@gmail.com>
2025-01-20 14:31:29 +0700

    add font configuration using google fonts

--- lib/core/config/theme/app_theme.dart
+++ lib/core/config/theme/app_theme.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:numusic/core/config/theme/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static final lighTheme = ThemeData(
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.lightBackground,
+    fontFamily: GoogleFonts.montserrat().fontFamily,
     brightness: Brightness.light,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
@@ -21,6 +23,7 @@ class AppTheme {
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.darkBackground,
     brightness: Brightness.dark,
+    fontFamily: GoogleFonts.montserrat().fontFamily,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
