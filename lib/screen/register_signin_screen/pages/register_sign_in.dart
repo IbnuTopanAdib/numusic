9a15cd00972999e1a7dde23819d2a53f306883a2
IbnuTopanAdib <resyaderesya@gmail.com>
2025-01-23 11:44:37 +0700

    feat : add appbar

--- lib/screen/register_signin_screen/pages/register_sign_in.dart
+++ lib/screen/register_signin_screen/pages/register_sign_in.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:numusic/common/widgets/appbar/general_app_bar.dart';
 import 'package:numusic/common/widgets/buttons/general_app_button.dart';
 import 'package:numusic/core/config/assets/app_images.dart';
 import 'package:numusic/core/config/assets/app_vectors.dart';
@@ -15,68 +16,96 @@ class RegisterSignIn extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          const GeneralAppBar(),
           Align(
-              alignment: Alignment.bottomRight,
-              child: Stack(
-                children: [
-                  Image.asset(AppImages.registerLoginPic),
-                  Positioned.fill(
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
-                      child: Container(color: Colors.black.withOpacity(0)),
+            alignment: Alignment.bottomRight,
+            child: Stack(
+              children: [
+                ShaderMask(
+                  shaderCallback: (Rect bounds) {
+                    return const LinearGradient(
+                      colors: [AppColors.primary, AppColors.primary],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ).createShader(bounds);
+                  },
+                  blendMode: BlendMode.srcATop,
+                  child: Image.asset(
+                    AppImages.registerLoginPic,
+                  ),
+                ),
+                Positioned.fill(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Transform.scale(
+                      scale: 0.98,
+                      child: Image.asset(
+                        AppImages.registerLoginPic,
+                      ),
                     ),
                   ),
-                ],
-              )),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(AppVectors.logo),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  'Enjoy Listening Music',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 18),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  'Lorem ipsum dolor sit amet amat jamet slepet slebew uhuy',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.grey,
-                      fontSize: 13),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      // Membatasi ukuran tombol agar fleksibel
-                      child: GeneralAppButton(
-                        onPressed: () {},
-                        title: 'Register',
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    Expanded(
-                      child: GeneralAppButton(
-                        onPressed: () {},
-                        title: 'Sign in',
-                      ),
-                    ),
-                  ],
                 ),
               ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(AppVectors.logo),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Enjoy Listening Music',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Lorem ipsum dolor sit amet amat jamet slepet slebew uhuy',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.grey,
+                        fontSize: 13),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: GeneralAppButton(
+                          onPressed: () {},
+                          title: 'Register',
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                          flex: 1,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Sign in',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          )),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
